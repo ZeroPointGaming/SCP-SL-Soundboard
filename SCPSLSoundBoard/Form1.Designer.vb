bac069a9d92dd50a3dd3a6ca8fa69477467bbcd0
ZeroPointGaming <comfyseven4@gmail.com>
2022-01-05 21:31:21 -0500

    Add files via upload

---
+++ SCPSLSoundBoard/Form1.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PlayAmbientSoundBtn = New System.Windows.Forms.Button()
+        Me.SCPSoundsBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ComboBox6 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Ambient 1 #20732", "Ambient 1 ", "Ambient 2 #20754", "Ambient 2", "Ambient 3 #20755", "Ambient 3", "Ambient 4 #20663", "Ambient 4", "Ambient 5 $20741", "Ambient 5", "Ambient 6 #20684", "Ambient 6", "Ambient 7 #20761", "Ambient 7", "Ambient 8 #20722", "Ambient 8", "Ambient 9", "Ambient 10", "Ambient 11", "Ambient 12", "Ambient 13", "Ambient 14", "Ambient 15", "Ambient 17", "Ambient 18", "Ambient 19", "Ambient 20", "Ambient 21", "Ambient 22", "Ambient 23"})
+        Me.ComboBox1.Location = New System.Drawing.Point(548, 108)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(181, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(548, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Ambient Sounds"
+        '
+        'PlayAmbientSoundBtn
+        '
+        Me.PlayAmbientSoundBtn.Location = New System.Drawing.Point(735, 108)
+        Me.PlayAmbientSoundBtn.Name = "PlayAmbientSoundBtn"
+        Me.PlayAmbientSoundBtn.Size = New System.Drawing.Size(75, 21)
+        Me.PlayAmbientSoundBtn.TabIndex = 2
+        Me.PlayAmbientSoundBtn.Text = "Play"
+        Me.PlayAmbientSoundBtn.UseVisualStyleBackColor = True
+        '
+        'SCPSoundsBtn
+        '
+        Me.SCPSoundsBtn.Location = New System.Drawing.Point(735, 68)
+        Me.SCPSoundsBtn.Name = "SCPSoundsBtn"
+        Me.SCPSoundsBtn.Size = New System.Drawing.Size(75, 21)
+        Me.SCPSoundsBtn.TabIndex = 5
+        Me.SCPSoundsBtn.Text = "Play"
+        Me.SCPSoundsBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(548, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "SCP Sounds"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"SCP 106 Contained", "SCP 106 Femur Breaker", "SCP 106 Footsteps 01", "SCP 106 Footsteps 02", "SCP 106 Footsteps 03", "SCP 173 Neck Snap 01", "SCP 173 Neck Snap 02", "SCP 173 Neck Snap 03", "SCP 173 Footsteps 01", "SCP 173 Footsteps 02", "SCP 173 Footsteps 03", "SCP 096 Idle Sound", "SCP 096 Trigger Sound", "SCP 096 Panic Sound"})
+        Me.ComboBox2.Location = New System.Drawing.Point(548, 68)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(181, 21)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(735, 25)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 21)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Play"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label3.Location = New System.Drawing.Point(548, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Scare Sounds"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"Scare 0", "Scare 1", "Scare 2", "Scare 3", "Scare 4", "Scare 5", "Scare 6"})
+        Me.ComboBox3.Location = New System.Drawing.Point(548, 25)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(181, 21)
+        Me.ComboBox3.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(735, 151)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 21)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Play"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label4.Location = New System.Drawing.Point(548, 135)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(87, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Weapon Sounds"
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Items.AddRange(New Object() {"ShotM249", "ShotMicroUzi", "ShotE11Rifle", "ShotPistol", "ReloadM249", "ReloadMicroUzi", "ReloadPistol", "ReloadE11Rifle01", "ReloadE11Rifle02", "EMPGrenade", "SmokeGrenade", "OutOfAmmo", "M-HID"})
+        Me.ComboBox4.Location = New System.Drawing.Point(548, 151)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(181, 21)
+        Me.ComboBox4.TabIndex = 9
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(735, 197)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 21)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Play"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label5.Location = New System.Drawing.Point(548, 181)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(96, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Nuke Alert Sounds"
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Items.AddRange(New Object() {"Detonation Canceled", "Main 80", "Main 90", "Main 100", "Main 110", "Main 120", "Resume 30", "Resume 40", "Resume 50", "Resume 60", "Resume 70", "Resume 80", "Resume 90", "Resume 100"})
+        Me.ComboBox5.Location = New System.Drawing.Point(548, 197)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(181, 21)
+        Me.ComboBox5.TabIndex = 12
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(735, 241)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 21)
+        Me.Button4.TabIndex = 17
+        Me.Button4.Text = "Play"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label6.Location = New System.Drawing.Point(548, 225)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(72, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Other Sounds"
+        '
+        'ComboBox6
+        '
+        Me.ComboBox6.FormattingEnabled = True
+        Me.ComboBox6.Items.AddRange(New Object() {"Noise 02", "Noise 07", "Chaos Insurgency Spawn", "Intercom Start", "Intercom Stop", "Spawn Bell", "Die Bell", "Fall Damage", "Helicopter"})
+        Me.ComboBox6.Location = New System.Drawing.Point(548, 241)
+        Me.ComboBox6.Name = "ComboBox6"
+        Me.ComboBox6.Size = New System.Drawing.Size(181, 21)
+        Me.ComboBox6.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(820, 375)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.ComboBox6)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ComboBox5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.SCPSoundsBtn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.PlayAmbientSoundBtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(836, 414)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(836, 414)
+        Me.Name = "Form1"
+        Me.Text = "SCP: Secret Laboratory Sound Board"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PlayAmbientSoundBtn As Button
+    Friend WithEvents SCPSoundsBtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ComboBox6 As ComboBox
+End Class
---
+++ SCPSLSoundBoard/Form1.vb
@@ -0,0 +1,231 @@
+﻿Public Class Form1
+    'My.Computer.Audio.Play(My.Resources. , AudioPlayMode.Background)
+
+    'Ambient Sounds
+    Private Sub PlayAmbientSoundBtn_Click(sender As Object, e As EventArgs) Handles PlayAmbientSoundBtn.Click
+        Try
+            Select Case ComboBox1.SelectedItem.ToString
+                Case "Ambient 1 #20732"
+                    My.Computer.Audio.Play(My.Resources.Ambient1a, AudioPlayMode.Background)
+                Case "Ambient 1 "
+                    My.Computer.Audio.Play(My.Resources.Ambient1, AudioPlayMode.Background)
+                Case "Ambient 2 #20754"
+                    My.Computer.Audio.Play(My.Resources.Ambient2a, AudioPlayMode.Background)
+                Case "Ambient 2"
+                    My.Computer.Audio.Play(My.Resources.Ambient2, AudioPlayMode.Background)
+                Case "Ambient 3 #20755"
+                    My.Computer.Audio.Play(My.Resources.Ambient3a, AudioPlayMode.Background)
+                Case "Ambient 3"
+                    My.Computer.Audio.Play(My.Resources.Ambient3, AudioPlayMode.Background)
+                Case "Ambient 4 #20663"
+                    My.Computer.Audio.Play(My.Resources.Ambient4a, AudioPlayMode.Background)
+                Case "Ambient 4"
+                    My.Computer.Audio.Play(My.Resources.Ambient4, AudioPlayMode.Background)
+                Case "Ambient 5 $20741"
+                    My.Computer.Audio.Play(My.Resources.Ambient5a, AudioPlayMode.Background)
+                Case "Ambient 5"
+                    My.Computer.Audio.Play(My.Resources.Ambient5, AudioPlayMode.Background)
+                Case "Ambient 6 #20684"
+                    My.Computer.Audio.Play(My.Resources.Ambient6a, AudioPlayMode.Background)
+                Case "Ambient 6"
+                    My.Computer.Audio.Play(My.Resources.Ambient6, AudioPlayMode.Background)
+                Case "Ambient 7 #20761"
+                    My.Computer.Audio.Play(My.Resources.Ambient7a, AudioPlayMode.Background)
+                Case "Ambient 7"
+                    My.Computer.Audio.Play(My.Resources.Ambient7, AudioPlayMode.Background)
+                Case "Ambient 8 #20722"
+                    My.Computer.Audio.Play(My.Resources.Ambient8a, AudioPlayMode.Background)
+                Case "Ambient 8"
+                    My.Computer.Audio.Play(My.Resources.Ambient8, AudioPlayMode.Background)
+                Case "Ambient 9"
+                    My.Computer.Audio.Play(My.Resources.Ambient9, AudioPlayMode.Background)
+                Case "Ambient 10"
+                    My.Computer.Audio.Play(My.Resources.Ambient10, AudioPlayMode.Background)
+                Case "Ambient 11"
+                    My.Computer.Audio.Play(My.Resources.Ambient11, AudioPlayMode.Background)
+                Case "Ambient 12"
+                    My.Computer.Audio.Play(My.Resources.Ambient12, AudioPlayMode.Background)
+                Case "Ambient 13"
+                    My.Computer.Audio.Play(My.Resources.Ambient13, AudioPlayMode.Background)
+                Case "Ambient 14"
+                    My.Computer.Audio.Play(My.Resources.Ambient14, AudioPlayMode.Background)
+                Case "Ambient 15"
+                    My.Computer.Audio.Play(My.Resources.Ambient15, AudioPlayMode.Background)
+                Case "Ambient 17"
+                    My.Computer.Audio.Play(My.Resources.Ambient17, AudioPlayMode.Background)
+                Case "Ambient 18"
+                    My.Computer.Audio.Play(My.Resources.Ambient18, AudioPlayMode.Background)
+                Case "Ambient 19"
+                    My.Computer.Audio.Play(My.Resources.Ambient19, AudioPlayMode.Background)
+                Case "Ambient 20"
+                    My.Computer.Audio.Play(My.Resources.Ambient20, AudioPlayMode.Background)
+                Case "Ambient 21"
+                    My.Computer.Audio.Play(My.Resources.Ambient21, AudioPlayMode.Background)
+                Case "Ambient 22"
+                    My.Computer.Audio.Play(My.Resources.Ambient22, AudioPlayMode.Background)
+                Case "Ambient 23"
+                    My.Computer.Audio.Play(My.Resources.Ambient22, AudioPlayMode.Background)
+            End Select
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    'SCP Sounds
+    Private Sub SCPSoundsBtn_Click(sender As Object, e As EventArgs) Handles SCPSoundsBtn.Click
+        Try
+            Select Case ComboBox2.SelectedItem.ToString
+                Case "SCP 106 Contained"
+                    My.Computer.Audio.Play(My.Resources.SCP106Contained, AudioPlayMode.Background)
+                Case "SCP 106 Femur Breaker"
+                    My.Computer.Audio.Play(My.Resources.SCP106FemurBreaker, AudioPlayMode.Background)
+                Case "SCP 106 Footsteps 01"
+                    My.Computer.Audio.Play(My.Resources.SCP106Footstep01, AudioPlayMode.Background)
+                Case "SCP 106 Footsteps 02"
+                    My.Computer.Audio.Play(My.Resources.SCP106Footstep02, AudioPlayMode.Background)
+                Case "SCP 106 Footsteps 03"
+                    My.Computer.Audio.Play(My.Resources.SCP106Footstep03, AudioPlayMode.Background)
+                Case "SCP 173 Neck Snap 01"
+                    My.Computer.Audio.Play(My.Resources.SCP173Snap01, AudioPlayMode.Background)
+                Case "SCP 173 Neck Snap 02"
+                    My.Computer.Audio.Play(My.Resources.SCP173Snap02, AudioPlayMode.Background)
+                Case "SCP 173 Neck Snap 03"
+                    My.Computer.Audio.Play(My.Resources.SCP173Snap03, AudioPlayMode.Background)
+                Case "SCP 173 Footsteps 01"
+                    My.Computer.Audio.Play(My.Resources.SCP173Footstep01, AudioPlayMode.Background)
+                Case "SCP 173 Footsteps 02"
+                    My.Computer.Audio.Play(My.Resources.SCP173Footstep02, AudioPlayMode.Background)
+                Case "SCP 173 Footsteps 03"
+                    My.Computer.Audio.Play(My.Resources.SCP173Footstep03, AudioPlayMode.Background)
+                Case "SCP 096 Idle Sound"
+                    My.Computer.Audio.Play(My.Resources.SCP096IdleSound, AudioPlayMode.Background)
+                Case "SCP 096 Trigger Sound"
+                    My.Computer.Audio.Play(My.Resources.SCP096Triggered, AudioPlayMode.Background)
+                Case "SCP 096 Panic Sound"
+                    My.Computer.Audio.Play(My.Resources.SCP096Scream, AudioPlayMode.Background)
+            End Select
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    'Scare Sounds
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Select Case ComboBox3.SelectedItem.ToString
+                Case "Scare 0"
+                    My.Computer.Audio.Play(My.Resources.Scare_0, AudioPlayMode.Background)
+                Case "Scare 1"
+                    My.Computer.Audio.Play(My.Resources.Scare_1, AudioPlayMode.Background)
+                Case "Scare 2"
+                    My.Computer.Audio.Play(My.Resources.Scare_2, AudioPlayMode.Background)
+                Case "Scare 3"
+                    My.Computer.Audio.Play(My.Resources.Scare_3, AudioPlayMode.Background)
+                Case "Scare 4"
+                    My.Computer.Audio.Play(My.Resources.Scare_4, AudioPlayMode.Background)
+                Case "Scare 5"
+                    My.Computer.Audio.Play(My.Resources.Scare_5, AudioPlayMode.Background)
+                Case "Scare 6"
+                    My.Computer.Audio.Play(My.Resources.Scare_6, AudioPlayMode.Background)
+            End Select
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    'Weapon Sounds
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Select Case ComboBox4.SelectedItem.ToString
+                Case "ShotM249"
+                    My.Computer.Audio.Play(My.Resources.ShotmM249, AudioPlayMode.Background)
+                Case "ShotMicroUzi"
+                    My.Computer.Audio.Play(My.Resources.ShotMicroUzi, AudioPlayMode.Background)
+                Case "ShotE11Rifle"
+                    My.Computer.Audio.Play(My.Resources.ShotE11Rifle, AudioPlayMode.Background)
+                Case "ShotPistol"
+                    My.Computer.Audio.Play(My.Resources.ShotPistol, AudioPlayMode.Background)
+                Case "ReloadM249"
+                    My.Computer.Audio.Play(My.Resources.ReloadM249, AudioPlayMode.Background)
+                Case "ReloadMicroUzi"
+                    My.Computer.Audio.Play(My.Resources.ReloadMicroUzi, AudioPlayMode.Background)
+                Case "ReloadPistol"
+                    My.Computer.Audio.Play(My.Resources.ReloadPistol, AudioPlayMode.Background)
+                Case "ReloadE11Rifle01"
+                    My.Computer.Audio.Play(My.Resources.ReloadE11Rifle01, AudioPlayMode.Background)
+                Case "ReloadE11Rifle02"
+                    My.Computer.Audio.Play(My.Resources.ReloadE11Rifle02, AudioPlayMode.Background)
+                Case "EMPGrenade"
+                    My.Computer.Audio.Play(My.Resources.EMPGrenade, AudioPlayMode.Background)
+                Case "SmokeGrenade"
+                    My.Computer.Audio.Play(My.Resources.SmokeGrenade, AudioPlayMode.Background)
+                Case "OutOfAmmo"
+                    My.Computer.Audio.Play(My.Resources.OutOfAmmo, AudioPlayMode.Background)
+                Case "M-HID"
+                    My.Computer.Audio.Play(My.Resources.HIDTurret, AudioPlayMode.Background)
+            End Select
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    'Nuke Sounds
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            Select Case ComboBox5.SelectedItem.ToString
+                Case "Detonation Canceled"
+                    My.Computer.Audio.Play(My.Resources.Canceled, AudioPlayMode.Background)
+                Case "Main 80"
+                    My.Computer.Audio.Play(My.Resources.Main80, AudioPlayMode.Background)
+                Case "Main 90"
+                    My.Computer.Audio.Play(My.Resources.Main90, AudioPlayMode.Background)
+                Case "Main 100"
+                    My.Computer.Audio.Play(My.Resources.Main100, AudioPlayMode.Background)
+                Case "Main 110"
+                    My.Computer.Audio.Play(My.Resources.Main110, AudioPlayMode.Background)
+                Case "Main 120"
+                    My.Computer.Audio.Play(My.Resources.Main120, AudioPlayMode.Background)
+                Case "Resume 30"
+                    My.Computer.Audio.Play(My.Resources.Resume30, AudioPlayMode.Background)
+                Case "Resume 40"
+                    My.Computer.Audio.Play(My.Resources.Resume40, AudioPlayMode.Background)
+                Case "Resume 50"
+                    My.Computer.Audio.Play(My.Resources.Resume50, AudioPlayMode.Background)
+                Case "Resume 60"
+                    My.Computer.Audio.Play(My.Resources.Resume60, AudioPlayMode.Background)
+                Case "Resume 70"
+                    My.Computer.Audio.Play(My.Resources.Resume70, AudioPlayMode.Background)
+                Case "Resume 80"
+                    My.Computer.Audio.Play(My.Resources.Resume80, AudioPlayMode.Background)
+                Case "Resume 90"
+                    My.Computer.Audio.Play(My.Resources.Resume90, AudioPlayMode.Background)
+                Case "Resume 100"
+                    My.Computer.Audio.Play(My.Resources.Resume100, AudioPlayMode.Background)
+            End Select
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    'Other Sounds
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            Select Case ComboBox6.SelectedItem.ToString
+                Case "Noise 02"
+                    My.Computer.Audio.Play(My.Resources.Broadcast2, AudioPlayMode.Background)
+                Case "Noise 07"
+                    My.Computer.Audio.Play(My.Resources.Broadcast7, AudioPlayMode.Background)
+                Case "Chaos Insurgency Spawn"
+                    My.Computer.Audio.Play(My.Resources.The_Chaos_Insurgency_Announce, AudioPlayMode.Background)
+                Case "Intercom Start"
+                    My.Computer.Audio.Play(My.Resources.ic_start, AudioPlayMode.Background)
+                Case "Intercom Stop"
+                    My.Computer.Audio.Play(My.Resources.ic_stop, AudioPlayMode.Background)
+                Case "Spawn Bell"
+                    My.Computer.Audio.Play(My.Resources.Bell1, AudioPlayMode.Background)
+                Case "Die Bell"
+                    My.Computer.Audio.Play(My.Resources.Bell2, AudioPlayMode.Background)
+                Case "Fall Damage"
+                    My.Computer.Audio.Play(My.Resources.falldamage, AudioPlayMode.Background)
+                Case "Helicopter"
+                    My.Computer.Audio.Play(My.Resources.Helicopter, AudioPlayMode.Background)
+            End Select
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
