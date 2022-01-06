6c3751c3e145d51961c0c80d8c1ddf0eaf3d2c4e
ZeroPointGaming <comfyseven4@gmail.com>
2022-01-05 21:38:30 -0500

    Add files via upload

---
+++ SCPSLSoundBoard/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SCPSL SoundBoard")>
+<Assembly: AssemblyDescription("SCP Soundboard for Secret Laboratory on Steam")>
+<Assembly: AssemblyCompany("LCHStudios")>
+<Assembly: AssemblyProduct("SCPSL SoundBoard")>
+<Assembly: AssemblyCopyright("Copyright ©  2018 LCHStudios")>
+<Assembly: AssemblyTrademark("LCHS")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e6c4ca3b-0d20-4523-91b2-57c8d9b6799f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
